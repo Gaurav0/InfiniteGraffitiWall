4d7f1edbfc290cd6dd8d41a24462d8fcc33cb57a
Gaurav Munjal <Gaurav0@aol.com>
2012-03-19 07:21:09 -0400

    Let cursor get clipped

--- js/graffiti.js
+++ js/graffiti.js
@@ -2,19 +2,20 @@ $(document).ready(function() {
 	var $body = $("body");
 	var $c = $("#c");
 	var $can = $("#can");
+	var $canimg = $("#can img");
 	
 	// Makes the can move around like the cursor
 	$body.mousemove(function(e) {
 	    var x = e.pageX;
 	    var y = e.pageY;
-	    var w = $can.width();
-	    var h = $can.height();
+	    var w = $canimg.width();
+	    var h = $canimg.height();
 	    var mx = $c.width();
 	    var my = $c.height();
-	    $can.css({
-	        left: x + w < mx ? x : mx - w,
-	        top: y + h < my ? y : my - h
-	    });
+	    var clipx = x + w > mx ? mx - x : w;
+	    var clipy = y + h > my ? my - y : h;
+	    var rect = "rect(0px " + clipx + "px " + clipy + "px 0px)";
+	    $can.css({ left: x, top: y, clip: rect });
 	});
 	
 	$body.mouseout(function(e) {
--- js/testgraffiti.js
+++ js/testgraffiti.js
@@ -9,12 +9,26 @@ $(document).ready(function() {
 	test("mousemove", function() {
 		$("body").simulate("mousemove", {
 			clientX: 200,
-			clientY: 100,
-			screenX: 200,
-			screenY: 100
+			clientY: 100
 		});
 		equal($("#can").css("left"), "200px");
 		equal($("#can").css("top"), "100px");
+	});
+	
+	test("clip", function() {
+		var w = $("#c").width();
+		var h = $("#c").height();
+		$("body").simulate("mousemove", {
+			clientX: w - 10,
+			clientY: h - 15
+		});
+		equal($("#can").css("left"), (w - 10) + "px");
+		equal($("#can").css("top"), (h - 15) + "px");
+		var rect = $("#can").css("clip").split(",");
+		equal(rect[1].trim(), "10px");
+		equal(rect[2].trim(), "15px");
+		equal($(window).width(), w);
+		equal($(window).height(), h);		
 	});
 	
 	test("mouseout", function() {
