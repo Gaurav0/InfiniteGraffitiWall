da08dab7952250d86a5055ab374832ce4feeb42b
Yuriy@Yuriy-Laptop <Yuriy@Yuriy-Laptop>
2012-03-23 14:46:46 -0400

    Now using jinja2 templates

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import webapp2
 import jinja2
 import os
 
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
-
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(''))
+		
+class TestPage(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('unittests.html')
+        self.response.out.write(template.render(''))
+		
+app = webapp2.WSGIApplication([('/', MainPage),('/unittests', TestPage)], debug=True)
