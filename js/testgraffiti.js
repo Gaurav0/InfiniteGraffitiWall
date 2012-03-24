1690c2df9be2b9310d17f3d21f93d0b2a68a963f
Gaurav Munjal <Gaurav0@aol.com>
2012-03-24 18:13:55 -0400

    Fix QUnit tests

--- js/testgraffiti.js
+++ js/testgraffiti.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 	module("can-cursor");
 	
 	test("mouseover", function() {
-		$("body").simulate("mouseover", {});
+		$("#wall").simulate("mouseover", {});
 		ok(!($("#can").is(":hidden")));
 	});
 	
 	test("mousemove", function() {
-		$("body").simulate("mousemove", {
+		$("#wall").simulate("mousemove", {
 			clientX: 200,
 			clientY: 100
 		});
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	test("clip", function() {
 		var w = $("#c").width();
 		var h = $("#c").height();
-		$("body").simulate("mousemove", {
+		$("#wall").simulate("mousemove", {
 			clientX: w - 10,
 			clientY: h - 15
 		});
@@ -31,12 +31,12 @@ $(document).ready(function() {
 			rect = rect.split(" ");
 		equal(rect[1].trim(), "10px");
 		equal(rect[2].trim(), "15px");
-		equal($(window).width(), w);
-		equal($(window).height(), h);		
+		equal($("#wall").width(), w);
+		equal($("#wall").height(), h);		
 	});
 	
 	test("mouseout", function() {
-		$("body").simulate("mouseout", {});
+		$("#wall").simulate("mouseout", {});
 		ok($("#can").is(":hidden"));
 	});
 	
@@ -44,35 +44,35 @@ $(document).ready(function() {
 	
 	test("scroll", function() {
 		$("#left").simulate("mouseover", {});
-		equal($("body").css("background-position"), "4px 0px");
+		equal($("#wall").css("background-position"), "4px 0px");
 		$("#left").simulate("mouseout", {});
 		
 		$("#right").simulate("mouseover", {});
-		equal($("body").css("background-position"), "0px 0px");
+		equal($("#wall").css("background-position"), "0px 0px");
 		$("#right").simulate("mouseout", {});
 
 		$("#top").simulate("mouseover", {});
-		equal($("body").css("background-position"), "0px 4px");
+		equal($("#wall").css("background-position"), "0px 4px");
 		$("#top").simulate("mouseout", {});
 
 		$("#bottom").simulate("mouseover", {});
-		equal($("body").css("background-position"), "0px 0px");
+		equal($("#wall").css("background-position"), "0px 0px");
 		$("#bottom").simulate("mouseout", {});
 
 		$("#top_left").simulate("mouseover", {});
-		equal($("body").css("background-position"), "3px 3px");
+		equal($("#wall").css("background-position"), "3px 3px");
 		$("#top_left").simulate("mouseout", {});
 
 		$("#top_right").simulate("mouseover", {});
-		equal($("body").css("background-position"), "0px 6px");
+		equal($("#wall").css("background-position"), "0px 6px");
 		$("#top_right").simulate("mouseout", {});
 
 		$("#bottom_right").simulate("mouseover", {});
-		equal($("body").css("background-position"), "-3px 3px");
+		equal($("#wall").css("background-position"), "-3px 3px");
 		$("#bottom_right").simulate("mouseout", {});
 
 		$("#bottom_left").simulate("mouseover", {});
-		equal($("body").css("background-position"), "0px 0px");
+		equal($("#wall").css("background-position"), "0px 0px");
 		$("#bottom_left").simulate("mouseout", {});
 	});
 	
@@ -80,11 +80,11 @@ $(document).ready(function() {
 	
 	/* Test does not work - simulate.js does not support mousewheel
 	test("mousewheel-scroll", function() {
-		$("body").simulate("mousewheel", {
+		$("#wall").simulate("mousewheel", {
 			deltaX: 1,
 			deltaY: 1
 		});
-		equal($("body").css("background-position"), "24px 24px");
+		equal($("#wall").css("background-position"), "24px 24px");
 	});
 	*/
 	
