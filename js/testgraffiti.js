2d83b340f463408b725d16cd8081de0faa568341
Yuriy@Yuriy-Laptop <Yuriy@Yuriy-Laptop>
2012-03-24 10:16:08 -0400

    Grouped all the screen movement tests

--- js/testgraffiti.js
+++ js/testgraffiti.js
@@ -40,49 +40,35 @@ $(document).ready(function() {
 		ok($("#can").is(":hidden"));
 	});
 	
-	test("left", function() {
+	test("screen_movement", function() {
 		$("#left").simulate("mouseover", {});
 		equal($("body").css("background-position"), "4px 0px");
 		$("#left").simulate("mouseout", {});
-	});
-	
-	test("right", function() {
+		
 		$("#right").simulate("mouseover", {});
 		equal($("body").css("background-position"), "0px 0px");
 		$("#right").simulate("mouseout", {});
-	});
-	
-	test("top", function() {
+		
 		$("#top").simulate("mouseover", {});
 		equal($("body").css("background-position"), "0px 4px");
 		$("#top").simulate("mouseout", {});
-	});
-	
-	test("bottom", function() {
+		
 		$("#bottom").simulate("mouseover", {});
 		equal($("body").css("background-position"), "0px 0px");
 		$("#bottom").simulate("mouseout", {});
-	});
-	
-	test("top_left", function() {
+		
 		$("#top_left").simulate("mouseover", {});
 		equal($("body").css("background-position"), "3px 3px");
 		$("#top_left").simulate("mouseout", {});
-	});
-	
-	test("top_right", function() {
+		
 		$("#top_right").simulate("mouseover", {});
 		equal($("body").css("background-position"), "0px 6px");
 		$("#top_right").simulate("mouseout", {});
-	});
-	
-	test("bottom_right", function() {
+		
 		$("#bottom_right").simulate("mouseover", {});
 		equal($("body").css("background-position"), "-3px 3px");
 		$("#bottom_right").simulate("mouseout", {});
-	});
-	
-	test("bottom_left", function() {
+		
 		$("#bottom_left").simulate("mouseover", {});
 		equal($("body").css("background-position"), "0px 0px");
 		$("#bottom_left").simulate("mouseout", {});
