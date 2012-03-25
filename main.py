e91cfd47676442ab355de3e397b40e49de370326
hery1989 <hratsim00@ccny.cuny.edu>
2012-03-25 12:03:51 -0400

    Added Login button.

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import webapp2
 import jinja2
 import os
 
+from google.appengine.api import users
+
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+	user = users.get_current_user()
+	if user:
+		login_url = users.create_login_url(self.request.uri)
+		login_label = 'logout'
+	else:
+		login_url = users.create_login_url(self.request.uri)
+		login_label = 'login'
+
         template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render(''))
+        self.response.out.write(template.render(login_url=login_url,login_label=login_label))
 		
 class TestPage(webapp2.RequestHandler):
     def get(self):
