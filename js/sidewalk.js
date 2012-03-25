d6f4fd72af09416a65ac94471d295a61ff5b2034
FMY22 <maluyoda22@gmail.com>
2012-03-25 16:54:35 -0400

    fixed to be able to merge

--- js/sidewalk.js
+++ js/sidewalk.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
-<<<<<<< local
-	
-=======
+
 	$("#sidewalk").append("<button type='button'>Size Picker</button>");
 	$("#sidewalk").append("<button type='button'>Color Picker</button>");
->>>>>>> other
+
 });
