782e0e987d78032a0fc4fa207616b4eef77a7ada
Gaurav Munjal <Gaurav0@aol.com>
2012-03-25 13:39:19 -0400

    Logout worked fine on dev server but not after upload with real google account. Fixed.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            login_url = users.create_login_url(self.request.uri)
+            login_url = users.create_logout_url(self.request.uri)
             login_label = 'logout'
         else:
             login_url = users.create_login_url(self.request.uri)
