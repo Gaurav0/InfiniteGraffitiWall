f4673a8e9c6592713aae4227cbc8a630ac357ba3
Yuriy@Yuriy-Laptop.home <Yuriy@Yuriy-Laptop.home>
2012-04-08 18:01:40 -0400

    The primary and secondary spray color can now switch and the effect is shown in the GUI

--- js/jquery.ps-color-picker.js
+++ js/jquery.ps-color-picker.js
@@ -443,14 +443,17 @@ ColorPicker.prototype._registerEvent = function(){
       $(this.linkedElement).bind("mousedown",function(event){_this.show();});
    }
    this.element.find(".old-color").click(function(){
+    ///////////////////////////////////////////
+    //My code here
+   //Store the current main color in a temporary variable
+    var tmpclr = _this.currentColor;
+   //Reset the main color to the secondary color
+    _this._restoreToInitial();
+   //Set the new secondary color to the new main color
+    _this.initialColor = tmpclr;
+   //Sets the color for the bottom box
+   _this.element.find(".old-color").css("backgroundColor",COLOR_SPACE.RGB2HEX(tmpclr));
    ///////////////////////////////////////////
-   //My code here
-   var tmpclr = _this.currentColor;
-   _this._restoreToInitial();
-   _this.initialColor = tmpclr;
-//      this.setColorText(this.currentColor);
-//      this.colorMap.setColor(this.currentColor);
- //     this.colorBar.setColor(this.currentColor);
    });
       
    $(this.canvasBar).add(this.canvasMap)
